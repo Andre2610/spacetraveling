b161dc336606879ec7b20c4e191909086f042fdf
Andre2610 <dre2610@gmail.com>
2020-07-14 10:23:46 +0200

    created user model

---
+++ models/user.js
@@ -0,0 +1,30 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define(
+    "user",
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {}
+  );
+  user.associate = function (models) {
+    // associations can be defined here
+  };
+  return user;
+};
