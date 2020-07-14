e01fb1e5da88fb6bc43f5192e5e7d92c78b18427
Andre2610 <dre2610@gmail.com>
2020-07-14 10:55:09 +0200

    created usertrip model

---
+++ migrations/20200714084959-create-usertrip.js
@@ -0,0 +1,44 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("usertrips", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+        allowNull: false,
+      },
+      tripId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "trips",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("usertrips");
+  },
+};
---
+++ models/usertrip.js
@@ -0,0 +1,33 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const trip = sequelize.define(
+    "trip",
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+        allowNull: false,
+      },
+      tripId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "trips",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+        allowNull: false,
+      },
+    },
+    {}
+  );
+  trip.associate = function (models) {
+    // associations can be defined here
+  };
+  return trip;
+};
