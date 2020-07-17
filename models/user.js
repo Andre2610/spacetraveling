72a62f695f2f99cd2ccc90a1f2b109a026fead0f
Andre2610 <dre2610@gmail.com>
2020-07-17 10:59:54 +0200

    added isAdmin column to user model

--- models/user.js
+++ models/user.js
@@ -24,6 +24,9 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.BOOLEAN,
         allowNull: false,
       },
+      isAdmin: {
+        type: DataTypes.BOOLEAN,
+      },
     },
     {}
   );
