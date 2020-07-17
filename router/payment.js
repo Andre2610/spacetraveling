59cd7cfdbfb591589fc12728da5eebd4fd034e4a
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-17 12:54:09 +0200

    modified email template to include planetName

--- router/payment.js
+++ router/payment.js
@@ -15,16 +15,17 @@ router.post("/", async (req, res, next) => {
     amount,
     tripId,
     departingDate,
-    planetId,
+    planetName,
     email,
-    userId,
     cardholder,
+    userId,
     travelClass,
   } = req.body;
   if (!id || !amount) {
     res.status(402).send("missing parameters!");
   }
 
+  console.log("WHAT IS THIS planetNAME:", planetName);
   try {
     const payment = await stripe.paymentIntents.create({
       amount,
@@ -47,7 +48,7 @@ router.post("/", async (req, res, next) => {
     const getBookingId = await Booking.findOne({
       where: { id: newBooking.id },
     });
-    console.log("WHAT IS MY BOOKINGID?");
+    console.log("WHAT IS MY BOOKINGID?:", getBookingId);
 
     //nodemailer
     const transporter = nodemailer.createTransport({
@@ -65,38 +66,31 @@ router.post("/", async (req, res, next) => {
       to: `${email}`,
       subject: `Space Travel inc. payment confirmation and boarding pass`,
       text: `BOOKING CONFIRMATION:Dear valued customer, thank you for choosing Space Travel inc as your off-planet spaceline.
+      This is your booking confirmation for ${planetName} and your BOARDINGPASS.
 
-      This is your booking confirmation for ${planetId} and your BOARDINGPASS.
-
-      Your flight to ${planetId} departs on ${departingDate}.
-
+      Your flight to ${planetName} departs on ${departingDate}.
+     
       You have chosen for ${travelClass}, should you wish to upgrade your seating please visit Seat-Upgrade
       
       IMPORTANT:Please bring a printed copy of this document to the check-in as it functions as your BOARDING PASS.
-      
-      
       `,
-      html: `<h3>BOOKING CONFIRMATION:</h3> <h2>Dear valued customer, thank you for choosing Space Travel inc as your off-planet spaceline</h2>
+      html: `
+      <div>
+      <img src="https://cdn.discordapp.com/attachments/718425463059513374/733635534924021760/bannermail.png"/>
+      </div>
+      <h3>BOOKING CONFIRMATION:</h3> <h2>Dear valued customer, thank you for choosing Space Travel inc as your off-planet spaceline</h2>
       <p>
-      This is your booking confirmation for ${planetId} and your <Strong>Boardingpass</Strong>.
+      This is your booking confirmation for ${planetName} and your <Strong>Boardingpass</Strong>.
       <br/>
-      Your flight to ${planetId} departs on ${departingDate}.
+      Your flight to ${planetName} departs on ${departingDate}.
       <br/>
-      You have chosen for ${travelClass} seating, should you wish to upgrade - please visit <a>Upgrade-Seating</a>
-      <br/>
-      <h3>IMPORTANT:</h3><p>Please bring a printed copy of this document to the check-in as it functions as youre <strong>Boarding Pass</strong></p>
-      
-      </p>`,
-    };
+      You have chosen for ${travelClass} seating, should you wish to upgrade - please visit <a href='www.google.com'>Upgrade-Seating</a>
+      </p>
+      <h3>IMPORTANT:</h3>
+      <p>Please bring a printed copy of this document to the check-in as it functions as youre <strong>Boarding Pass</strong></p>
 
-    // first Name
-    // last Name
-    // add bookingId
-    // User
-    // Travel Class
-    // Origin = Earth
-    // Destination = dynamic
-    // Date of departure
+      `,
+    };
 
     console.log(
       "WAHT IS CONFIRMATIONANDTICKETEMAIL",
